c1b52257ca1710fd8ebca953cfcecd27a23e5d39
Thomas Lee <tms.lee9@gmail.com>
2026-01-15 16:32:22 -0500

    set up tests for kvstore

--- tests/kvstore_test.cpp
+++ tests/kvstore_test.cpp
@@ -1,3 +1,60 @@
-int main() {
-    return 0;
+#include "kvstore/kvstore.hpp"
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace kvstore::test {
+
+class KVStoreTest : public ::testing::Test {
+protected:
+    KVStore store;
+};
+
+TEST_F(KVStoreTest, InitiallyEmpty) {
+
+}
+
+TEST_F(KVStoreTest, PutAndGet) {
+
+}
+
+TEST_F(KVStoreTest, GetMissingKey) {
+    
+}
+
+TEST_F(KVStoreTest, PutOverwrites) {
+    
+}
+
+TEST_F(KVStoreTest, Contains) {
+    
+}
+
+TEST_F(KVStoreTest, Remove) {
+    
+}
+
+TEST_F(KVStoreTest, Clear) {
+    
+}
+
+TEST_F(KVStoreTest, Size) {
+    
+}
+
+TEST_F(KVStoreTest, EmptyKeyAndValue) {
+    
+}
+
+TEST_F(KVStoreTest, ConcurrentWrites) {
+    
+}
+
+TEST_F(KVStoreTest, ConcurrentReadsAndWrites) {
+    
+}
+
 }
