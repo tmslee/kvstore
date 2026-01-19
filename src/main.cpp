8d7293a7fbe87839e768858652694e43bbe42c7c
Thomas Lee <tms.lee9@gmail.com>
2026-01-19 11:51:19 -0500

    made main.cpp that runs the server

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include "kvstore/kvstore.hpp"
+#include "kvstore/server.hpp"
+
+#include <csignal>
+#include <iostream>
+
+namespace {
+kvstore::Server* g_server = nullptr;
+
+void signal_handler(int) {
+    if(g_server != nullptr) {
+        g_server->stop();
+    }
+}
+
+}// namespace
+
+int main() {
+    kvstore::Options store_opts;
+    store_opts.persistence_path = "data.wal";
+
+    kvstore::KVStore store(store_ops);
+
+    kvstore::ServerOptions server_opts;
+    server_opts.port = 6379;
+
+    kvstore::Server server(store, server_opts);
+    g_server = &server;
+
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIFTERM, signal_handler);
+
+    std::cout << "Starting kvstore server on port " << server_opts.port << "...\n";
+    server.start();
+    std::cout << "Server running. press Ctrl+C to stop.\n";
+
+    while(server.running()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    std::cout << "Server stopped.\n";
+    return 0;
+}
