2d1780d9364ad285bff774143c31bfd81b8bdbfa
Thomas Lee <tms.lee9@gmail.com>
2026-01-23 10:43:51 -0500

    implemented signal handler src

---
+++ src/util/signal_handler.cpp
@@ -0,0 +1,42 @@
+#include "kvstore/util/signal_handler.hpp"
+
+#include <csignal>
+#include <condition_variable>
+#include <mutex>
+
+namespace kvstore::util {
+
+std::atomic<bool> SignalHandler::shutown_requested_{false};
+
+namespace {
+
+std::mutex shutdown_mutex;
+std::condition_variable shutdown_cv;
+
+void signal_handler(int signal) {
+    (void)signal;
+    SignalHandler::request_shutdown();
+}
+
+} //namespace
+
+void SignalHandler::install {
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGTERM, signal_handler);
+}
+
+bool SignalHandler::should_shutdown() {
+    return shutdown_requested_.load();
+}
+
+void SignalHandler::wait_for_shutdown() {
+    std::unique_lock lock(shutdown_mutex);
+    shutdown_cv.wait(lock, []{return shutdown_requested_.load();});
+}
+
+void SignalHandler::request_shutdown() {
+    shutdown_requested_.store(true);
+    shutdown_cv.notify_all();
+}
+
+} //namespace kvstore::util
