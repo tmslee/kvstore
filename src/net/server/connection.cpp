7f648d3560009ec33045216ed2df0996d677f521
Thomas Lee <tms.lee9@gmail.com>
2026-02-03 09:58:16 -0500

    included algorithm and removed static cast from previous commit

--- src/net/server/connection.cpp
+++ src/net/server/connection.cpp
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <cerrno>
 
 #include "kvstore/net/binary_protocol.hpp"
@@ -135,7 +136,7 @@ std::optional<Request> Connection::try_parse_request() {
     } else {
         // text protocol: look for newline (search from offset)
         auto start = read_buffer_.begin() + read_offset_;
-        auto it = std::find(start, read_buffer_.end(), static_cast<unsigned char>('\n'));
+        auto it = std::find(start, read_buffer_.end(), '\n');
         if (it == read_buffer_.end()) {
             return std::nullopt;
         }
